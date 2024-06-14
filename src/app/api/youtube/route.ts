5a16981a073452ef9ff4425c9e47bcef91a57947
Yusuke Hakamaya <yhakamaya1031@gmail.com>
2024-06-14 09:38:47 +0900

    add API route to fetch recent videos

---
+++ src/app/api/youtube/route.ts
@@ -0,0 +1,58 @@
+import { Video } from "@/interfaces/video";
+import { google } from "googleapis";
+import { NextResponse } from "next/server";
+
+const youtube = google.youtube({
+  version: "v3",
+  auth: process.env.YOUTUBE_DATA_API_KEY,
+});
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const channelId = searchParams.get("channelId");
+
+  if (!channelId) {
+    return NextResponse.json(
+      { error: "The channelId parameter is required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const channelResponse = await youtube.channels.list({
+      part: ["contentDetails"],
+      id: [channelId],
+    });
+
+    const uploadsPlaylistId =
+      channelResponse.data.items?.[0]?.contentDetails?.relatedPlaylists
+        ?.uploads;
+
+    if (!uploadsPlaylistId) {
+      return NextResponse.json(
+        { error: "Uploads playlist not found" },
+        { status: 404 }
+      );
+    }
+
+    const playlistResponse = await youtube.playlistItems.list({
+      part: ["snippet", "player"],
+      playlistId: uploadsPlaylistId,
+      maxResults: 4,
+    });
+
+    const videos: Video[] =
+      playlistResponse.data.items?.map((item) => ({
+        title: item.snippet?.title ?? "",
+        videoId: item.snippet?.resourceId?.videoId ?? "",
+        publishedAt: item.snippet?.publishedAt ?? "",
+      })) ?? [];
+
+    return NextResponse.json(videos);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
