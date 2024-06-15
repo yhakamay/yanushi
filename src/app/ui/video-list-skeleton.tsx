0df4477789bb3a858d06c9d31f56980a92b1efe3
Yusuke Hakamaya <yhakamaya1031@gmail.com>
2024-06-16 00:27:04 +0900

    update length to 12 from 4

--- src/app/ui/video-list-skeleton.tsx
+++ src/app/ui/video-list-skeleton.tsx
@@ -1,7 +1,7 @@
 export default function VideoListSkelton() {
   return (
     <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4 mb-8">
-      {Array.from({ length: 4 }).map((_, index) => (
+      {Array.from({ length: 12 }).map((_, index) => (
         <li className="flex flex-col" key={index}>
           <div className="skeleton aspect-video rounded-lg mb-4"></div>
           <div className="skeleton rounded-lg h-6 mb-1"></div>
